7762cf255af99fa1f691c48001d93a07971ec038
Shchepetkov <maksim.shchepetkov1995@gmail.com>
2020-10-10 20:11:19 +0400

    An example of how sprite animation works.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Animation Sprite on JavaFX
+
+This app was created specifically to show how animation can work in javaFx rather than gif.
+How animation works in GIFs can be viewed in this application 
+    [tamagotchi](https://github.com/Shchepetkov/tamagotchi.git).
+___
---
+++ src/main/java/test/Main.java
@@ -0,0 +1,28 @@
+package test;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application
+    {
+        @Override public void start(Stage primaryStage) throws IOException
+            {
+                Parent root = FXMLLoader.load(getClass().getResource("/Begin.fxml"));
+                Scene scene = new Scene(root);
+
+                primaryStage.setScene(scene);
+                primaryStage.setOnCloseRequest(t -> System.exit(0));
+                primaryStage.setTitle("Game");
+                primaryStage.show();
+            }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+    }
